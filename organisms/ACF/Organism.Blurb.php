3b5ca5b41e629d41c94ad44f8a6750c3f4fbb129
Josh Nederveld <joshnederveld@gmail.com>
2016-05-09 14:20:53 -0400

    Updated Blurb Organism

--- organisms/ACF/Organism.Blurb.php
+++ organisms/ACF/Organism.Blurb.php
@@ -9,14 +9,18 @@ class ACF_Blurb extends OrganismTemplate {
 		if ( ! isset( $data['name'] ) ) {
 			$this->name = 'acf-blurb';
 		}
-		
+
 		parent::__construct( $data );
-		
+
 		$this->structure = [
 			'inside' => [
 				'parts' => [
 					'image' => [
-						'atom' => 'image'
+						'tag_type' => 'false_without_content',
+					],
+					'icon'  => [
+						'tag_type' => 'false_without_content',
+						'content'  => ''
 					],
 					'title' => [
 						'tag'      => 'h3',
@@ -38,13 +42,14 @@ class ACF_Blurb extends OrganismTemplate {
 			]
 		];
 
+		$media_position_class = 'position--' . $data['media_placement'];
+
 		/*——————————————————————————————————————————
 		/  Image- handled separately because it's multiple pieces.
 		——————————————————————————————————————————*/
 		if ( '' !== $data['foreground_image'] ) {
 
-			$attachment_id        = '';
-			$image_position_class = $data['media_placement'];
+			$attachment_id = '';
 
 			if ( is_int( $data['foreground_image'] ) ) {
 				$attachment_id = $data['foreground_image'];
@@ -57,12 +62,21 @@ class ACF_Blurb extends OrganismTemplate {
 			if ( '' !== $attachment_id ) {
 
 				$this->structure['inside']['parts']['image'] = [
+					'atom'          => 'Image',
 					'attachment_id' => $attachment_id,
-					'class'         => [ $image_position_class ]
+					'class'         => [ $media_position_class ]
 				];
 			} else {
 				unset( $this->structure['inside']['parts']['image'] );
 			}
+		}
+
+		/*——————————————————————————————————————————
+		/  Icon generates after check
+		——————————————————————————————————————————*/
+		if ( '' !== $data['icon_name'] ) {
+			$this->structure['inside']['parts']['icon']['content'] = Utility::getSvgIcon( $data['icon_name'] );
+			$this->structure['inside']['parts']['icon']['class']   = $media_position_class;
 		}
 	}
 }
