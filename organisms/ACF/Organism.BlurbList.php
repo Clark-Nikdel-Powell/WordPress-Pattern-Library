72c0a906afe1a140097371603b69d3bfdc1eb31b
Josh Nederveld <joshnederveld@gmail.com>
2016-05-09 11:33:32 -0400

    Setup

--- organisms/ACF/Organism.BlurbList.php
+++ organisms/ACF/Organism.BlurbList.php
@@ -5,13 +5,11 @@ class ACF_Blurb_list extends OrganismTemplate {
 
 	public function __construct( $data ) {
 
-		// Set the name before the parent construct so that default classes can get added.
 		if ( !isset( $data['name'] ) ) {
 			$this->name = 'acf-blurblist';
 		}
 
 		parent::__construct( $data );
-
 	}
 
 }
