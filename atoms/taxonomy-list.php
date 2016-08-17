35be937054ab784221626e555e9c60de06edf5b4
Josh Nederveld <joshnederveld@gmail.com>
2016-08-17 10:37:24 -0400

    Upgraded to provide a list without links, if preferred

--- atoms/taxonomy-list.php
+++ atoms/taxonomy-list.php
@@ -16,6 +16,7 @@ class TaxonomyList extends AtomTemplate {
 	public $before;
 	public $separator;
 	public $after;
+	public $include_links;
 
 	public function __construct( $data ) {
 
@@ -40,8 +41,23 @@ class TaxonomyList extends AtomTemplate {
 		$this->separator = isset( $data['separator'] ) ? $data['separator'] : ', ';
 		$this->after     = isset( $data['after'] ) ? $data['after'] : '';
 
-		$this->tag     = isset( $data['tag'] ) ? $data['tag'] : 'p';
-		$this->content = get_the_term_list( $this->post_object->ID, $this->taxonomy, $this->before, $this->separator, $this->after );
+		$this->include_links = isset( $data['include-links'] ) ? $data['include-links'] : true;
 
+		$this->tag = isset( $data['tag'] ) ? $data['tag'] : 'p';
+
+		if ( true === $this->include_links ) {
+			$this->content = get_the_term_list( $this->post_object->ID, $this->taxonomy, $this->before, $this->separator, $this->after );
+		} else {
+			$terms_arr      = get_the_terms( $this->post_object->ID, $this->taxonomy );
+			$term_names_arr = array();
+
+			foreach ( $terms_arr as $term_obj ) {
+				$term_names_arr[] = $term_obj->name;
+			}
+
+			$terms_list = implode( $this->separator, $term_names_arr );
+
+			$this->content = $terms_list;
+		}
 	}
 }
