d3b808c57f9fdc7fc00c2d5f28257b1aa7c07860
Josh Nederveld <joshnederveld@gmail.com>
2016-08-22 09:59:21 -0400

    Change before/after args so it doesn't collide with atom arguments

--- atoms/taxonomy-list.php
+++ atoms/taxonomy-list.php
@@ -13,9 +13,9 @@ namespace CNP;
 class TaxonomyList extends AtomTemplate {
 
 	public $taxonomy;
-	public $before;
+	public $before_list;
 	public $separator;
-	public $after;
+	public $after_list;
 	public $include_links;
 
 	public function __construct( $data ) {
@@ -37,16 +37,16 @@ class TaxonomyList extends AtomTemplate {
 			$this->taxonomy = $data['taxonomy'];
 		}
 
-		$this->before    = isset( $data['before'] ) ? $data['before'] : '';
+		$this->before_list    = isset( $data['before-list'] ) ? $data['before-list'] : '';
 		$this->separator = isset( $data['separator'] ) ? $data['separator'] : ', ';
-		$this->after     = isset( $data['after'] ) ? $data['after'] : '';
+		$this->after_list     = isset( $data['after-list'] ) ? $data['after-list'] : '';
 
 		$this->include_links = isset( $data['include-links'] ) ? $data['include-links'] : true;
 
 		$this->tag = isset( $data['tag'] ) ? $data['tag'] : 'p';
 
 		if ( true === $this->include_links ) {
-			$this->content = get_the_term_list( $this->post_object->ID, $this->taxonomy, $this->before, $this->separator, $this->after );
+			$this->content = get_the_term_list( $this->post_object->ID, $this->taxonomy, $this->before_list, $this->separator, $this->after_list );
 		} else {
 			$terms_arr      = get_the_terms( $this->post_object->ID, $this->taxonomy );
 			$term_names_arr = array();
@@ -57,7 +57,7 @@ class TaxonomyList extends AtomTemplate {
 
 			$terms_list = implode( $this->separator, $term_names_arr );
 
-			$this->content = $terms_list;
+			$this->content = $this->before_list . $terms_list . $this->after_list;
 		}
 	}
 }
